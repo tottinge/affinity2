8be43371c8fa8c9bc9de99d89ff366f23b65615c
Tim Ottinger <tottinge@industriallogic.com>
2018-03-12 20:23:38 -0400

    break into functions

--- affinity2.py
+++ affinity2.py
@@ -7,22 +7,27 @@ from itertools import combinations
 from gitlogparser import GitLogParser
 
 
-# Do GIT LOG
-edge_collector = Counter()
-parser = GitLogParser()
-with subprocess.Popen(['git','log','--numstat'], stdout=subprocess.PIPE, universal_newlines=True) as command:
-    for commit in parser.feed(command.stdout):
+
+def collect_edge_weights(feed):
+    edge_collector = Counter()
+    parser = GitLogParser()
+    for commit in parser.feed(feed):
         for edge in combinations(commit, 2):
             edge_collector[edge]+=1
+    return edge_collector
 
-g = networkx.Graph()
-for edge,weight in edge_collector.items():
-    g.add_edge(edge, weight)
+def build_graph(collection):
+    g = networkx.Graph()
+    for edge,weight in collection.items():
+        g.add_edge(edge, weight)
+    return g
 
+# Edge collection and parser feeding need to be separate functions.
 
+def main():
+    logSource = subprocess.Popen(['git', 'log', '--numstat'], stdout=subprocess.PIPE, universal_newlines=True)
+    n = collect_edge_weights(logSource.stdout)
+    g = build_graph(n)
 
-# Okay, we have nodes and weights in the files_in_commits, so convert to a graph
-# g.add_edges_from(  ... something cool here .... )
-# ... and then do something cool
-            
+# Next we need to do some kind of interesting analysis
 
